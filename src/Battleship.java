ebea21976abcf014d3cd00caa9fe5678df3c74b3
Z-ltn <karolyizoli@gmail.com>
2021-05-03 16:52:12 +0200

    feat: board placement check

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,8 +1,14 @@
+import board.Board;
 import board.Square;
+
+import java.util.Arrays;
 
 public class Battleship {
     public static void main(String[] args) {
         Square cica = new Square(1, 1);
         System.out.println(cica.getStatus());
+        System.out.println();
+        Board kutya = new Board();
+        System.out.println(kutya.print());
     }
 }
--- src/board/Board.java
+++ src/board/Board.java
@@ -1,14 +1,28 @@
 package board;
 
+import java.util.Arrays;
+
 public class Board {
     private final Square[][] ocean;
     private final int BOARD_SIZE = 10;
 
     public Board() {
         this.ocean = new Square[BOARD_SIZE][BOARD_SIZE];
+        for (int i=0;i<BOARD_SIZE;i++) {
+            for (int j=0;j<BOARD_SIZE;j++) {
+                ocean[i][j] = new Square(i, j);
+            }
+        }
     }
 
-    public boolean isItOnBoard(int[] placement) {
-        return placement[0] > this.BOARD_SIZE && placement[1] > this.BOARD_SIZE ;
+    public boolean isPlacementOk(int[] placement) {
+        int positionX = placement[0];
+        int positionY = placement[1];
+        return positionX < this.BOARD_SIZE && positionY < this.BOARD_SIZE && // Is it on board?
+        this.ocean[positionX][positionY].getStatus().equals("EMPTY"); // DO NOT put it on other ships
+    }
+
+    public String print() {
+        return Arrays.deepToString(this.ocean);
     }
 }
