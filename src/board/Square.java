113e637621c5d98fb39c2720b40848bf37a0b096
Z-ltn <karolyizoli@gmail.com>
2021-05-03 16:03:33 +0200

    feat: square class methods

--- src/board/Square.java
+++ src/board/Square.java
@@ -1,10 +1,46 @@
 package board;
 
 public class Square {
-    enum SquareStatus {
+    private enum SquareStatus {
         EMPTY,
+        SHIP,
         HIT,
-        MISS,
-        SUNK
+        MISS;
+        public String getCharacter() {
+            return this.equals(EMPTY) ? "🔥" : this.equals(SHIP) ? "🚀" :
+                    this.equals(HIT) ? "🌊" : "🌪";
+        };
+    }
+    private boolean isShip, isHit;
+    private final int x, y;
+
+    public Square(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.isShip = false;
+        this.isHit = false;
+    }
+
+    public void setShip() {
+        isShip = true;
+    }
+
+    public void setHit() {
+        isHit = true;
+    }
+
+    public boolean isShip() {
+        return isShip;
+    }
+
+    public boolean isHit() {
+        return isHit;
+    }
+
+    public String getStatus() {
+        if (isShip && isHit) return SquareStatus.HIT.getCharacter();
+        else if (isShip) return SquareStatus.SHIP.getCharacter();
+        else if (isHit) return SquareStatus.MISS.getCharacter();
+        else return SquareStatus.EMPTY.getCharacter();
     }
 }
