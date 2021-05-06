fcd89098314e2f0547bdaf099ff51dd1afb38e14
KizsGergely <hu.gergely@gmail.com>
2021-05-06 13:39:27 +0200

    add board placement mode

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -91,24 +91,23 @@ public class Game {
         }
 
 
-    public void pewPew() {
-        while (true) {
+        public void pewPew () {
+            while (true) {
+            }
+        }
+
+        public static void runGame () {
 
         }
-    }
+        //generate board full of water
+        //Setting phase:
+        //Set the 5 ships in an order (choose starting point-endpoint (check in between)/ then second player
+        //hide ships
+        //Shooting phase:
+        //Player choose a coordinate on the opponents board, check if its a hit, change icon,change player
+        // check if every ship sunk
+        //Player x Win
 
-    public static void runGame() {
-
-    }
-    //generate board full of water
-    //Setting phase:
-    //Set the 5 ships in an order (choose starting point-endpoint (check in between)/ then second player
-    //hide ships
-    //Shooting phase:
-    //Player choose a coordinate on the opponents board, check if its a hit, change icon,change player
-    // check if every ship sunk
-    //Player x Win
-}
 
         private void randomGameplay (Board board1, Board board2){
             for (int j = 0; j < 2; j++) {
